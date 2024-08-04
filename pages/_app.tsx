e30c77c41b3a451a9dbe4249e9b1460e31632508
ozer <ozer@90pixel.com>
2024-08-04 15:45:10 +0300

    ADD: email provider and main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { ThemeProvider } from '@/components/theme-provider';
+import '@/styles/globals.css';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,83 @@
-import { Inter } from 'next/font/google';
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] });
+import { Exo } from 'next/font/google';
+import { FaReact } from 'react-icons/fa';
+
+import { ModeToggle } from '@/components/ModeToggle';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+
+const exo = Exo({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} />;
+  const [subject, setSubject] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('/api/send', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ subject }),
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        console.log('Email sent successfully:', result);
+      } else {
+        console.error('Error sending email:', result);
+      }
+    } catch (error) {
+      console.error('Unexpected error:', error);
+    }
+  };
+
+  return (
+    <main className={`flex h-screen flex-col items-center p-24 pt-10 pb-2 ${exo.className}`}>
+      <div className="absolute right-5">
+        <ModeToggle />
+      </div>
+      <div className="text-4xl mb-2 font-bold flex items-center justify-center gap-2">
+        <h2>ReactBlog</h2>
+        <FaReact className="text-blue-500 w-10 h-10 animate-spin-slow" />
+      </div>
+      <p className="text-center text-lg mb-5">Submit links to articles, repositories, or anything related to React!</p>
+      <div className="flex gap-2">
+        <Input
+          placeholder="Enter email subject"
+          value={subject}
+          onChange={e => setSubject(e.target.value)}
+          className="mx-auto w-96"
+        />
+        <Button onClick={handleSubmit}>Submit</Button>
+      </div>
+      <div />
+      <Pagination className="flex-1">
+        <PaginationContent className="items-end pb-10">
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </main>
+  );
 }
